5c42f28fe0f092ceb6c0d34b92042a3e60761515
mhamza6000 <57058620+mhamza6000@users.noreply.github.com>
2023-04-19 10:02:26 +0100

    Update resnet.py

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -136,7 +136,7 @@ class ResNet(nn.Module):
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 256, layers[3], stride=2)
         self.layer5 = self._make_layer(block, 512, layers[4], stride=last_stride)
 
