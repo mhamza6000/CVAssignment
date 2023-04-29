6ffbe860ed3b774ee893d7559b47d75461a9f31a
mhamza6000 <57058620+mhamza6000@users.noreply.github.com>
2023-04-29 23:49:55 +0100

    Update args.py

--- args.py
+++ args.py
@@ -105,19 +105,7 @@ def argument_parser():
     parser.add_argument(
         "--weight-decay", default=5e-03, type=float, help="weight decay"
     )
-    # Adamax
-    parser.add_argument(
-        "--optim",
-        type=str,
-        default="adamax",
-        help="optimization algorithm (see optimizers.py)",
-    )
-    parser.add_argument(
-        "--lr", default=0.0001, type=float, help="initial learning rate"
-    )
-    parser.add_argument(
-        "--weight-decay", default=5e-03, type=float, help="weight decay"
-    )
+   
     # sgd
     parser.add_argument(
         "--momentum",
