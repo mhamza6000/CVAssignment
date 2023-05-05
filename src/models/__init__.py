68fb15307dcc218d8b462298cde7be628bb4547d
mhamza6000 <57058620+mhamza6000@users.noreply.github.com>
2023-05-05 23:40:55 +0100

    Update __init__.py

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -9,7 +9,6 @@ from .resnet import (
     resnet50_fc512,
 )
 from .tvmodels import mobilenet_v3_small, vgg16
-from .siamese_network import SiameseNetwork
 
 
 __model_factory = {
@@ -22,7 +21,6 @@ __model_factory = {
     "resnet50_fc512": resnet50_fc512,
     "mobilenet_v3_small": mobilenet_v3_small,
     "vgg16": vgg16,
-    "siamese_network":SiameseNetwork
 }
 
 
