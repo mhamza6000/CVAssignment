4419492a7aa03f8a1f112d566a13517569df92be
mhamza6000 <57058620+mhamza6000@users.noreply.github.com>
2023-05-08 18:15:09 +0100

    Update resnet.py

--- src/models/resnet.py
+++ src/models/resnet.py
@@ -124,18 +124,18 @@ class ResNet(nn.Module):
         dropout_p=None,
         **kwargs,
     ):
-        self.inplanes = 64
+        self.inplanes = 128
         super().__init__()
         self.loss = loss
         self.feature_dim = 512 * block.expansion
 
         # backbone network
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.conv1 = nn.Conv2d(3, 128, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(128)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        #self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer1 = self._make_layer(block, 128, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=last_stride)
 
